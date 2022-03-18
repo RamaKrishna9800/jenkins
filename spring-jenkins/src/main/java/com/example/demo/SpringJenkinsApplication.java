5f5151d837d06646375b851bca29857f9d354c9c
Gangadhara.G <Gangadhara.G@EXIND3010-120.allfon.in>
2022-03-18 18:04:18 +0530

    jenkin example

---
+++ spring-jenkins/src/main/java/com/example/demo/SpringJenkinsApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+@RequestMapping
+public String getMessage() {
+	return "hello";
+}
+}

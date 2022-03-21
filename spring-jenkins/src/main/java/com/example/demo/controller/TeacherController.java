c7f4274793b171eef12ba567ba917266323bd3f1
Gangadhara.G <Gangadhara.G@EXIND3010-120.allfon.in>
2022-03-21 12:47:50 +0530

    new method added

--- spring-jenkins/src/main/java/com/example/demo/controller/TeacherController.java
+++ spring-jenkins/src/main/java/com/example/demo/controller/TeacherController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,14 @@ public class TeacherController {
  @GetMapping(path ="/teachers")
  public String getTeachers() {
 	 List<String> list =Arrays.asList("ram","raji","sam");
+	 return list.toString();
+ }
+ @GetMapping(path ="/teachers/{dept}")
+ public String getTeachersByDepartment(@PathVariable("dept") String dept) {
+	 List<String> list =Arrays.asList("ram","raji","sam");
+	 if(dept.equals("ece")){
+		 list = Arrays.asList("a","b","c");
+	 }
 	 return list.toString();
  }
 }
